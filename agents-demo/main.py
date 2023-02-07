1f579c7cf8f6fa2a60b1e11962b261e7a391f1ff
Nitish Gautam <nitishgautam.uob@gmail.com>
2023-02-07 17:34:29 +0000

    added time feature and random automated send topic

--- agents-demo/main.py
+++ agents-demo/main.py
@@ -1,10 +1,16 @@
+# import libraries
 import faust
 
+# create the app name
 app = faust.App("agents-demo",)
 
-greetings_topic = app.topic("greetings", value_type=str, value_serializer='raw')
+# From the app send topic to greeting topic
+greetings_topic = app.topic(
+    "greetings", value_type=str, value_serializer='raw')
 
+
+# wait for the streaming data to be consumed from greeting topic and print the result. Please note the function is async
 @app.agent(greetings_topic)
 async def greetings_processor(stream):
-  async for greeting in stream:
-    print(f"Greeting is '{greeting}'")
+    async for greeting in stream:
+        print(f"Greeting is '{greeting}'")
